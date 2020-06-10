2f328efe37306e88a52df3675e8aa865c8f2ac20
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-10 14:26:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,27 @@
-def join_nested_strings(src)
+# def join_nested_strings(src)
+# row_index = 0
+# new_string = []
+#   while row_index < src.count do
+#     element_index = 0
+#     while element_index < src[row_index].count do
+#       if src[row_index][element_index].class == String
+#         new_string << src[row_index][element_index]
+#       end
+#       element_index += 1
+#     end
+#     row_index += 1
+#   end
+#   new_string.join(' ')
+# end
+
+new_string []
 row_index = 0
-new_string = []
-  while row_index < src.count do
-    element_index = 0
-    while element_index < src[row_index].count do
-      if src[row_index][element_index].class == String
-        new_string << src[row_index][element_index]
-      end
-      element_index += 1
-    end
+while row_index < src.count do
+  element_index = 0
+  while element_index < src[row_index.count] do
+    element_index += 1
+    new_string << [row_index].grep(String).join(" ")
     row_index += 1
   end
-  new_string.join(' ')
+  new_string
 end
