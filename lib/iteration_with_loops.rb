69f09f1e7da267739dbd0e2e850a09f0ca0a345a
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-10 14:29:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -15,13 +15,14 @@
 # end
 
 def join_nested_strings(src)
-new_string = []
+new_string = " "
 row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index.count] do
     element_index += 1
-    new_string << [row_index].grep(String).join(" ")
+  end
+    new_string += src[row_index].grep(String).join(" ")
     row_index += 1
   end
   new_string.join(" ")
